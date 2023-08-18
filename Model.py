4dd7226409c39edf88e126e0430d2b6b0a5816b1
Y. Qiang Sun <85260799+yqsun91@users.noreply.github.com>
2023-08-17 22:51:49 -0500

    Add files via upload

--- Model.py
+++ Model.py
@@ -65,26 +65,6 @@ class FullyConnected(nn.Module):
 
         return self.linear_stack(X)
 
-class EarlyStopper:
-    def __init__(self, patience=1, min_delta=0):
-        self.patience = patience
-        self.min_delta = min_delta
-        self.counter = 0
-        self.min_validation_loss = np.inf
-
-    def early_stop(self, validation_loss):
-        if validation_loss < self.min_validation_loss:
-            self.min_validation_loss = validation_loss
-            self.counter = 0
-
-            #save model
-            torch.save(model.state_dict(), 'conv_torch.pth')
-
-        elif validation_loss > (self.min_validation_loss + self.min_delta):
-            self.counter += 1
-            if self.counter >= self.patience:
-                return True
-        return False
 
 # training loop
 def train_loop(dataloader, model, loss_fn, optimizer):
